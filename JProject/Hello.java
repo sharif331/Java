286ce867cfe684927e405ccd3b9bc9fb8122b412
Sharif-Personel <sharif331@gmail.com>
2018-08-14 18:06:00 +0530

    Anshul

--- JProject/Hello.java
+++ JProject/Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 public static void main(String[] args){
 for(int i=0;i<30;i++){
-    System.out.println("Values:::Sharif"+i);
+    System.out.println("Values:::Anshul"+i);
 }
 }
 }
