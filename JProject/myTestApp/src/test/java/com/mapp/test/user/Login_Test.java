236e52e031465196543ab9841c6afa1494bddd89
Sharif-Personel <sharif331@gmail.com>
2018-08-26 08:20:55 +0530

    MyTestApp

---
+++ JProject/myTestApp/src/test/java/com/mapp/test/user/Login_Test.java
@@ -0,0 +1,18 @@
+package com.mapp.test.user;
+
+import org.testng.annotations.Test;
+
+import com.mapp.obj.common.BrowserLaunch;
+
+
+
+public class Login_Test extends BrowserLaunch{
+	
+	
+	@Test 
+	public void ok(){
+		
+		Login_Test user=new Login_Test();
+		
+	}
+}
