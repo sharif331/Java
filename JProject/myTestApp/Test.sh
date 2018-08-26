06c353381f663712cff10a0c0919a67c10b8180a
Sharif-Personel <sharif331@gmail.com>
2018-08-26 10:39:57 +0530

    Updated

--- JProject/myTestApp/Test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-projectLocation=/Users/sharif/Desktop/Automation/Deliver/NeonMob
-cd $projectLocation
-classpath=$projectLocation/lib/*:$projectLocation/target/classes
-java -cp $classpath org.testng.TestNG $projectLocation/TestNg.xml
---
+++ src/test/java/com/spec/obj/posts/FirstPost_Test.java
@@ -0,0 +1,34 @@
+package com.spec.obj.posts;
+
+import org.testng.annotations.Test;
+
+import com.mapp.obj.common.BrowserLaunch;
+
+public class FirstPost_Test extends BrowserLaunch{
+
+	
+	
+	@Test
+	public void createPost() throws InterruptedException{
+		FirstPost post=new FirstPost(driver);
+		Thread.sleep(3000);
+//		assertTrue(post.isNewPostLinkDisplay(), "New Post Link not Displayed");
+//		post.clickOnNewPost();
+//	//	assertTrue(post.isTitleFieldDisplay(), "Title field not displayed");
+//		post.waitForTitel();
+//		post.enterTitle("MyFirstPost");
+//	//	assertTrue(post.isPostDescriptionFieldDisplay(), "Description field is not displyed");
+//		post.enterDescription("description");
+//	//	assertTrue(post.isBodyFieldDisplay(), "Body field is not displayed");
+//		post.enterBody("body");
+//	//	assertTrue(post.isSaveButtonDisplay(), "Save button not displayed");
+//		post.clickOnSaveButton();
+////		assertTrue(post.isBackButtonDisplay(), "Back button not found");
+//		post.clickOnBackButton();
+		assertTrue(post.isPostCreated("MyFirstPost"), "Post is not created ");
+	//	post.getByXpath();
+		post.clickOnSortBytitle();
+		Thread.sleep(3000);
+		//ok
+	}
+}
